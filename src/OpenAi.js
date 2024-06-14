785751198808a7f18d5d6ab9ab38bcf7212bda51
sathishakarapu <akarapusathish003@gmail.com>
2024-06-14 13:07:20 +0530

    first-commit

--- src/OpenAi.js
+++ src/OpenAi.js
@@ -1,20 +1,20 @@
-// OpenAi.js
-import { OpenAI } from 'openai';
+import { OpenAI } from "openai";
+const appid = "sk-proj-tDF0JXH6emIogNZDQ3AOT3BlbkFJzwNWUKnazGcNlvHuF2nf";
 
 const openai = new OpenAI({
-    apiKey: "sk-proj-tDF0JXH6emIogNZDQ3AOT3BlbkFJzwNWUKnazGcNlvHuF2nf",
-    dangerouslyAllowBrowser: true
+  apiKey: appid,
+  dangerouslyAllowBrowser: true,
 });
 
 export async function sendMsgOpenAi(message) {
-    const res = await openai.completions.create({
-        model: 'gpt-3.5-turbo',
-        prompt: message,
-        temperature: 0.7,
-        max_tokens: 256,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0
-    });
-    return res.choices[0].text;
+  const res = await openai.completions.create({
+    model: "gpt-3.5-turbo",
+    prompt: message,
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  return res.choices[0].text;
 }
